b94f43cda8d7160b4a01e70be4443644c02b195c
Victor Rendina <vrendina@gmail.com>
2017-05-27 23:06:19 -0400

    Added error handling

--- app/src/main/java/io/levelsoftware/carculator/ui/vehiclelist/VehicleListFragment.java
+++ app/src/main/java/io/levelsoftware/carculator/ui/vehiclelist/VehicleListFragment.java
@@ -16,10 +16,17 @@
 
 package io.levelsoftware.carculator.ui.vehiclelist;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
@@ -31,7 +38,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,6 +47,7 @@ import io.levelsoftware.carculator.data.CarculatorContract;
 import io.levelsoftware.carculator.sync.BaseIntentService;
 import io.levelsoftware.carculator.sync.SyncBroadcastReceiver;
 import io.levelsoftware.carculator.sync.vehicle.VehicleIntentService;
+import io.levelsoftware.carculator.util.NetworkUtils;
 import timber.log.Timber;
 
 
@@ -52,14 +60,19 @@ public class VehicleListFragment extends Fragment implements
 
     @BindView(R.id.swipe_refresh_layout) SwipeRefreshLayout swipeRefreshLayout;
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.image_view_status) ImageView statusImageView;
 
-    private SyncBroadcastReceiver receiver;
+    private SyncBroadcastReceiver syncReceiver;
+    private BroadcastReceiver networkReceiver;
+
     private VehicleListContainerAdapter adaper;
 
     private String searchQuery = "";
-    private boolean syncError = false;
+
+    private Snackbar errorSnackbar;
 
     public int count;
+    private boolean initialLoadComplete = false;
 
     public VehicleListFragment() {}
 
@@ -80,11 +93,14 @@ public class VehicleListFragment extends Fragment implements
         adaper = new VehicleListContainerAdapter();
         recyclerView.setAdapter(adaper);
 
+        showVehicleList(false);
+        hideStatusImage();
+
         if(savedInstanceState != null) {
             searchQuery = savedInstanceState.getString(getString(R.string.intent_key_search_query));
         }
 
-        bindSyncReceiver();
+        bindReceivers();
         getActivity().getSupportLoaderManager().initLoader(KEY_VEHICLE_LOADER, null, this);
 
         return view;
@@ -93,7 +109,7 @@ public class VehicleListFragment extends Fragment implements
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        unBindSyncReceiver();
+        unbindReceivers();
     }
 
     @Override
@@ -104,6 +120,7 @@ public class VehicleListFragment extends Fragment implements
 
     @Override
     public void onRefresh() {
+        Timber.d("Called onRefresh");
         loadVehicleData();
     }
 
@@ -121,12 +138,16 @@ public class VehicleListFragment extends Fragment implements
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         count = data.getCount();
-        if(count == 0 && !syncError) {
+        initialLoadComplete = true;
+
+        if(shouldLoadVehicleData()) {
             loadVehicleData();
         } else {
-            swipeRefreshLayout.setRefreshing(false);
+            setSwipeRefreshLayoutState(false, false);
+
             adaper.setCursor(data);
             adaper.filter(searchQuery);
+            showVehicleList(true);
         }
     }
 
@@ -136,55 +157,126 @@ public class VehicleListFragment extends Fragment implements
     }
 
     private void loadVehicleData() {
-        syncError = false;
-        swipeRefreshLayout.setRefreshing(true);
+        Timber.d("Loading vehicle data...");
+        VehicleIntentService.start(getActivity());
 
-        if(!VehicleIntentService.isRunning()) {
-            VehicleIntentService.start(getActivity());
+        setSwipeRefreshLayoutState(true, true);
+        hideError();
+    }
+
+    private boolean shouldLoadVehicleData() {
+        // If we already have data, we shouldn't try to load any
+        if(count > 0) {
+            return false;
         }
+        // If we haven't finished running the loader to check for existing data
+        if(!initialLoadComplete) {
+            return false;
+        }
+        // If the vehicle service is already running don't load
+        return !VehicleIntentService.isRunning();
     }
 
-    private void bindSyncReceiver() {
-        receiver = new SyncBroadcastReceiver(this);
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(receiver,
+    private void bindReceivers() {
+        syncReceiver = new SyncBroadcastReceiver(this);
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(syncReceiver,
                 new IntentFilter(getString(R.string.broadcast_sync_vehicle)));
+
+        networkReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(NetworkUtils.networkIsAvailable(context)) {
+                    if(shouldLoadVehicleData()) {
+                        loadVehicleData();
+                    }
+                }
+            }
+        };
+        getContext().registerReceiver(networkReceiver,
+                new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
     }
 
-    private void unBindSyncReceiver() {
-        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(receiver);
+    private void unbindReceivers() {
+        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(syncReceiver);
+        getContext().unregisterReceiver(networkReceiver);
     }
 
     @Override
     public void statusSuccess() {
         Timber.d("RECEIVED: Status broadcast success");
-        swipeRefreshLayout.setEnabled(false);
-        swipeRefreshLayout.setRefreshing(false);
+        setSwipeRefreshLayoutState(false, false);
     }
 
     @Override
     public void statusError(int code, @Nullable String message) {
         Timber.d("RECEIVED: Status broadcast error");
-        syncError = true;
         if(count == 0) {
             showError(code);
         }
     }
 
+    private void showStatusImage(@DrawableRes int drawable) {
+        statusImageView.setImageResource(drawable);
+        statusImageView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideStatusImage() {
+        statusImageView.setVisibility(View.GONE);
+    }
+
+    private void showVehicleList(boolean visible) {
+        int visibility = visible ? View.VISIBLE : View.GONE;
+        recyclerView.setVisibility(visibility);
+    }
+
+    private void setSwipeRefreshLayoutState(boolean enabled, boolean refreshing) {
+        swipeRefreshLayout.setEnabled(enabled);
+        swipeRefreshLayout.setRefreshing(refreshing);
+    }
+
     private void showError(int code) {
-        swipeRefreshLayout.setEnabled(true);
-        swipeRefreshLayout.setRefreshing(false);
+        setSwipeRefreshLayoutState(true, false);
+        showVehicleList(false);
 
         switch(code) {
             case BaseIntentService.STATUS_ERROR_NO_NETWORK:
-                Toast.makeText(getActivity(), "No Network", Toast.LENGTH_LONG).show();
+                showErrorSnackbar(R.string.error_no_network);
+                showStatusImage(R.drawable.ic_logo_no_network);
                 break;
 
             case BaseIntentService.STATUS_ERROR_NETWORK_ISSUE:
-                Toast.makeText(getActivity(), "Network Problem", Toast.LENGTH_LONG).show();
+                showErrorSnackbar(R.string.error_network_error);
+                showStatusImage(R.drawable.ic_logo_no_network);
                 break;
 
             default:
-                Toast.makeText(getActivity(), "Error Loading Data", Toast.LENGTH_LONG).show();
+                showErrorSnackbar(R.string.error_data);
+                showStatusImage(R.drawable.ic_logo_error);
+        }
+    }
+
+    private void hideError() {
+        hideStatusImage();
+
+        if(errorSnackbar != null) {
+            errorSnackbar.dismiss();
+        }
+    }
+
+    private void showErrorSnackbar(@StringRes int messageId) {
+        View view = getView();
+        if(view != null) {
+            errorSnackbar = Snackbar.make(view, messageId, Snackbar.LENGTH_INDEFINITE);
+            errorSnackbar.setAction(R.string.retry, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    loadVehicleData();
+                }
+            });
+
+            errorSnackbar.getView().setBackgroundColor(ContextCompat.getColor(getContext(), R.color.snackbarBackground));
+            errorSnackbar.setActionTextColor(ContextCompat.getColor(getContext(), R.color.snackbarActionText));
+            errorSnackbar.show();
         }
     }
 
@@ -192,6 +284,12 @@ public class VehicleListFragment extends Fragment implements
         this.searchQuery = query;
         if(query != null && count > 0) {
             adaper.filter(query);
+
+            if(adaper.getItemCount() == 0) {
+                showStatusImage(R.drawable.ic_logo_no_results);
+            } else {
+                hideStatusImage();
+            }
         }
     }
 }
